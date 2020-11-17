5cfd7c19b6984e5a6afb59ddc1deb0bd60ec28cc
Arthur <arthur.rochette@gmail.com>
2020-11-18 00:10:52 +0100

    try tp fix thread
    pb with multiple printing.

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -19,13 +19,12 @@ bool modif = true;
 float buff = 0, buff2 = 0;
 clock_t t;
 
-const auto processor_count = std::thread::hardware_concurrency();
-std::array<int, windowSize * windowSize> pixelsImage;
-std::array<sf::Uint8, windowSize * windowSize * 4> pixels;
+const auto processor_count = 1;                                 //std::thread::hardware_concurrency();
+sf::Uint8 *pixels = new sf::Uint8[windowSize * windowSize * 4]; //rgba
 
 int mandelBrotColor(int x, int y, float cr = 0, float cy = 0)
 {
-    complex<float> a((float)(x + camerax) / (float)windowSize * zoom - cr, (y + cameray) / (float)windowSize * zoom - cy); //*2 dezoom
+    complex<float> a((float)(x + camerax) / (float)(windowSize * zoom - cr), (y + cameray) / (float)(windowSize * zoom - cy)); //*2 dezoom
     complex<float> z(0, 0);
 
     int i = 0;
@@ -42,95 +41,101 @@ int mandelBrotColor(int x, int y, float cr = 0, float cy = 0)
 
 void imageThread(int nmthread)
 {
+    int red_code;
 
-    float redCode;
-    int nbrpixel = windowSize / processor_count;
-    for (int x = nbrpixel * nmthread; x < nbrpixel * (nmthread + 1); x++)
+    int nbrpixel = (4 * windowSize) / processor_count;
+
+    cout << "hhh" << nbrpixel * (nmthread + 1) << "   " << nbrpixel * nmthread << endl;
+
+    for (int y = 0; y < windowSize; y++)
     {
-        for (int y = 0; y < windowSize; y++)
+        for (int x = nbrpixel * nmthread; x < nbrpixel * (nmthread + 1); x += 4)
         {
-            redCode = mandelBrotColor(x, y, buff, buff2);
-            m.lock();
 
-            pixelsImage[x + y * windowSize] = redCode;
+            red_code = mandelBrotColor(x / 4, y, buff, buff2);
+
+            m.lock();
+            pixels[x + y * windowSize] = red_code;
+            pixels[x + y * windowSize + 1] = 0;
+            pixels[x + y * windowSize + 2] = 0;
+            pixels[x + y * windowSize + 3] = 255;
             m.unlock();
         }
+    }
+        for (int i = 0; i < windowSize*4; i++)
+    {
+        pixels[i] = 255;
     }
 }
-
-void windowThread(int nmthread)
+void writeImage2File()
 {
-    float red_code;
-    int nbrpixel = windowSize / processor_count;
-
-    for (int x = nbrpixel * nmthread; x < nbrpixel * (nmthread + 1); x++)
+    ofstream my_image("Mandelbrot.ppm");
+    if (my_image.is_open())
     {
+        my_image << "P3\n"
+                 << windowSize << " " << windowSize << " 255\n";
         for (int y = 0; y < windowSize; y++)
         {
-            red_code = mandelBrotColor(x, y, buff, buff2);
-
-            m.lock();
-
-            pixels.at(x + y * windowSize) = red_code;
-            pixels.at(x + y * windowSize + 1) = 0;
-            pixels.at(x + y * windowSize + 2) = 0;
-            pixels.at(x + y * windowSize + 3) = 255;
-            m.unlock();
+            for (int x = 0; x < (4 * windowSize); x += 4)
+            {
+                my_image << (int)pixels[x + y * windowSize] << ' ' << (int)pixels[x + y * windowSize + 1] << ' ' << (int)pixels[x + y * windowSize + 2] << "\n";
+            }
         }
+        my_image.close();
     }
+    t = clock() - t;
+    cout << "time : " << t << "ms";
+    return;
+}
+
+void createImage()
+{
+    std::vector<std::thread> threadlist;
+    for (int i = 0; i < processor_count; i++)
+    {
+        threadlist.emplace_back(imageThread, i);
+    }
+    for (int i = 0; i < processor_count; i++)
+    {
+        threadlist.at(i).join();
+        cerr << "thread " << i << " joined" << endl;
+    }
+    threadlist.clear();
 }
 
 int main(int argc, char *argv[])
 {
     sf::Texture texture;
-    sf::Sprite sprite;
+    if (!texture.create(windowSize, windowSize))
+    {
+        cerr << "Failed create Texture" << endl;
+        //bitch
+    }
+    sf::Sprite sprite(texture);
     sf::Image image;
     sf::Vector2i mouseposfirst, mousepossecond;
     int nbpoints = 10000;
     float buff = 0, buff2 = 0;
     bool followmouse = false;
     t = clock();
-    std::vector<std::thread> threadlist;
 
     if (argc > 1)
     {
 
         if (strcmp(argv[1], "-image") == 0)
         {
-            cout << "set to image mod" << endl;
-            for (int i = 0; i < processor_count; i++)
-            {
-                threadlist.emplace_back(imageThread, i);
-            }
-            for (int i = 0; i < processor_count; i++)
-            {
-                threadlist.at(i).join();
-                cerr << "thread " << i << " joined" << endl;
-            }
-
-            ofstream my_image("Mandelbrot.ppm");
-            if (my_image.is_open())
-            {
-                my_image << "P3\n"
-                         << windowSize << " " << windowSize << " 255\n";
-                for (int y = 0; y < windowSize; y++)
-                {
-                    for (int x = 0; x < windowSize; x++)
-                    {
-                        my_image << pixelsImage.at(x + y * windowSize) << ' ' << 0 << ' ' << 0 << "\n";
-                    }
-                }
-
-                my_image.close();
-            }
-            t = clock() - t;
-            cout << "time : " << t << "ms";
-            return 0;
+            createImage();
+            writeImage2File();
+            return -1;
         }
     }
 
     sf::RenderWindow window(sf::VideoMode(windowSize, windowSize), "Mandelbrot");
     bool dede = true;
+    createImage();
+    /*sprite.setOrigin(windowSize/2, windowSize/2);
+    sprite.setPosition(windowSize/2, windowSize/2);
+    sprite.rotate(90);*/
     while (window.isOpen())
     {
         sf::Event event;
@@ -185,26 +190,13 @@ int main(int argc, char *argv[])
         if (modif)
         {
             //call thread
-
-            for (int i = 0; i < processor_count; i++)
-            {
-                threadlist.emplace_back(windowThread, i);
-            }
-            for (int i = 0; i < processor_count; i++)
-            {
-                threadlist.at(i).join();
-                cerr << "thread " << i << " joined" << endl;
-            }
-            threadlist.clear();
+            createImage();
             modif = false;
         }
 
         //Creer image correspondant au tableau de pixels
 
-        image.create(windowSize, windowSize, pixels.data());
-
-        texture.loadFromImage(image);
-        sprite.setTexture(texture);
+        texture.update(pixels);
 
         window.clear(sf::Color::Black);
         window.draw(sprite);
