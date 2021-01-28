8ca04704d69ce7569068fe08faebe7298508b314
Arthur <arthur.rochette@gmail.com>
2021-01-28 14:10:36 +0100

    try to fix

--- Mandelbrot.cpp
+++ Mandelbrot.cpp
@@ -1,203 +1,124 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
-#include <math.h>
 #include <vector>
 #include <complex>
 #include <fstream>
 #include <thread>
 #include <mutex>
-#include <string.h>
 
 using namespace std;
 
 const int windowSize = 300;
-float zoom = 1;
-float camerax = 0, cameray = 0;
+const int MAX_ITERATION = 20;
 std::mutex m;
-bool modif = true;
-float buff = 0, buff2 = 0;
-clock_t t;
 
-const auto processor_count = 1;                                 //std::thread::hardware_concurrency();
+
+const auto processor_count = 1;
 sf::Uint8 *pixels = new sf::Uint8[windowSize * windowSize * 4]; //rgba
 
-int mandelBrotColor(int x, int y, float cr = 0, float cy = 0)
+std::vector<std::thread *> threadlist;
+
+struct rgba{
+    int r;
+    int g;
+    int b;
+    int a;
+};
+
+rgba mandelBrotColor(long double x, long double y, double cr = 1, double cy = 1)
 {
-    complex<float> a((float)(x + camerax) / (float)(windowSize * zoom - cr), (y + cameray) / (float)(windowSize * zoom - cy)); //*2 dezoom
-    complex<float> z(0, 0);
+    rgba colors;
+    int brightness;
+    complex<long double> a((long double)(x) / (long double)(windowSize - cr), (y ) / (long double)(windowSize - cy));
+    complex<long double> z(0, 0);
 
     int i = 0;
-    while (abs(z) < 2 && i <= 20)
+    while (abs(z) < 2 && i <= MAX_ITERATION)
     {
         z = z * z + a; //z²+c
         i++;
     }
-    if (i < 20)
-        return (255 * i) / 20;
+    if (i < MAX_ITERATION )
+        brightness = (255 * i) / MAX_ITERATION;
     else
-        return 0;
+        brightness = 0;
+
+    colors.r = brightness;
+    if(colors.r > 255){
+        colors.r = 255;
+    }
+    colors.g = 0;
+    colors.b = 0;
+    colors.a = 255;
+    return colors;
 }
 
-void imageThread(int nmthread)
-{
-    int red_code;
-
-    int nbrpixel = (4 * windowSize) / processor_count;
-
-    cout << "hhh" << nbrpixel * (nmthread + 1) << "   " << nbrpixel * nmthread << endl;
-
-    for (int y = 0; y < windowSize; y++)
-    {
-        for (int x = nbrpixel * nmthread; x < nbrpixel * (nmthread + 1); x += 4)
-        {
-
-            red_code = mandelBrotColor(x / 4, y, buff, buff2);
-
+void draw(int index) {
+    for (int y = 0; y < windowSize*4; y++) {
+        for (int x = index * (windowSize/processor_count); x < ((windowSize * 4)/processor_count)*index; x++) {
+            rgba colors = mandelBrotColor(x,y);
+            if(colors.r > 240) cout << "ahoy!!"<<endl;
             m.lock();
-            pixels[x + y * windowSize] = red_code;
-            pixels[x + y * windowSize + 1] = 0;
-            pixels[x + y * windowSize + 2] = 0;
-            pixels[x + y * windowSize + 3] = 255;
+            pixels[x + y * windowSize] = colors.r;//r
+            pixels[x + y * windowSize +1] = colors.g;//g
+            pixels[x + y * windowSize +2 ] = colors.b;//b
+            pixels[x + y * windowSize +4] =colors.a;//a
             m.unlock();
         }
     }
-        for (int i = 0; i < windowSize*4; i++)
-    {
-        pixels[i] = 255;
+}
+
+void createThreadList() {
+    for (int i = 0; i < processor_count; i++) {
+        threadlist.push_back(new std::thread(draw, i));
+        std::cout << "Thread " << i << " created " << endl;
     }
 }
-void writeImage2File()
-{
-    ofstream my_image("Mandelbrot.ppm");
-    if (my_image.is_open())
-    {
-        my_image << "P3\n"
-                 << windowSize << " " << windowSize << " 255\n";
-        for (int y = 0; y < windowSize; y++)
-        {
-            for (int x = 0; x < (4 * windowSize); x += 4)
-            {
-                my_image << (int)pixels[x + y * windowSize] << ' ' << (int)pixels[x + y * windowSize + 1] << ' ' << (int)pixels[x + y * windowSize + 2] << "\n";
-            }
+
+void syncThreadList() {
+    for (int i = 0; i < processor_count; i++) {
+        threadlist.at(i)->join();
+        std::cout << "Thread " << i << " joined " << endl;
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    cout << "CPU core: " << processor_count << endl;
+    createThreadList();
+
+    syncThreadList();
+    if(argc == 1){
+
+
+        ofstream  file;
+        file.open("fractal.txt");
+        file << "P3\n" << windowSize << " " << windowSize << " 255\n";
+        for(int i = 0 ; i < windowSize*windowSize*4; i+=4){
+            file << (int)pixels[i] << " " << (int)pixels[i] << " " << (int)pixels[i] << "\n";
         }
-        my_image.close();
+        file.close();
+        cout << "image done" << endl;
+        return 1;
     }
-    t = clock() - t;
-    cout << "time : " << t << "ms";
-    return;
-}
 
-void createImage()
-{
-    std::vector<std::thread> threadlist;
-    for (int i = 0; i < processor_count; i++)
-    {
-        threadlist.emplace_back(imageThread, i);
-    }
-    for (int i = 0; i < processor_count; i++)
-    {
-        threadlist.at(i).join();
-        cerr << "thread " << i << " joined" << endl;
-    }
-    threadlist.clear();
-}
-
-int main(int argc, char *argv[])
-{
     sf::Texture texture;
-    if (!texture.create(windowSize, windowSize))
-    {
+    if (!texture.create(windowSize, windowSize)) {
         cerr << "Failed create Texture" << endl;
-        //bitch
     }
     sf::Sprite sprite(texture);
     sf::Image image;
-    sf::Vector2i mouseposfirst, mousepossecond;
-    int nbpoints = 10000;
-    float buff = 0, buff2 = 0;
-    bool followmouse = false;
-    t = clock();
-
-    if (argc > 1)
-    {
-
-        if (strcmp(argv[1], "-image") == 0)
-        {
-            createImage();
-            writeImage2File();
-            return -1;
-        }
-    }
-
     sf::RenderWindow window(sf::VideoMode(windowSize, windowSize), "Mandelbrot");
-    bool dede = true;
-    createImage();
-    /*sprite.setOrigin(windowSize/2, windowSize/2);
-    sprite.setPosition(windowSize/2, windowSize/2);
-    sprite.rotate(90);*/
-    while (window.isOpen())
-    {
+
+    while (window.isOpen()) {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
-            if (event.type == sf::Event::MouseWheelScrolled)
-            {
-                if (event.mouseWheelScroll.delta == 1)
-                {
-
-                    zoom += zoom / 1.5f;
-                    modif = true;
-                }
-                else
-                {
-                    if (zoom - 1 / zoom <= 0)
-                    {
-                        zoom = zoom / 1.5f;
-                        modif = true;
-                    }
-                    else
-                    {
-                        zoom -= zoom / 1.5f;
-                        modif = true;
-                    }
-                }
-            }
-            if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::Up)
-                {
-                    camerax -= 1;
-                }
-                else if (event.key.code == sf::Keyboard::Down)
-                {
-                    camerax += 1;
-                }
-                else if (event.key.code == sf::Keyboard::Left)
-                {
-                    cameray -= 1;
-                }
-                else
-                {
-                    cameray += 1;
-                }
-                modif = true;
-            }
         }
-
-        if (modif)
-        {
-            //call thread
-            createImage();
-            modif = false;
-        }
-
-        //Creer image correspondant au tableau de pixels
 
         texture.update(pixels);
-
         window.clear(sf::Color::Black);
         window.draw(sprite);
         window.display();
